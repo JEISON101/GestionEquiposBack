4b6be3e4ff8f5165c4d4a18dc13b1bca45109d05
Jesus <jesuscastrojaramillo346@gmail.com>
2025-05-22 16:00:55 -0500

    error

--- app/controller/EquipoController.ts
+++ app/controller/EquipoController.ts
@@ -2,23 +2,29 @@ import client from "../database/pgDatabase";
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class equiposcontroller{
-    async obtenerEquipos({request,response}){
+    async getEquipo({request,response}){
         const result = await client.query('select * from equipo')
         console.log(result.rows)
         return response.json({ mensaje: 'Estos son los equipos', datos: result.rows })
     }
 
-    async crearEquipo({request,response}){
+    async postEquipo({request,response}){
         const {nombre,anio_fundacion}=request.body()
         await client.query(`insert into equipo(nombre,anio_fundacion)values($1,$2)`,[nombre,anio_fundacion])
         return response.json({mensaje:'Se creo el equipo', datos:{nombre,anio_fundacion},
         })
     }
 
-    async actualizarEquipo({params, request, response}){
+    async putEquipo({params, request, response}){
         const id = params.id
         const {nombre,anio_fundacion} = request.body()
         const result = await client.query('update equipo set nombre=$,anio_fundacion=$2 where id=$3',[nombre,anio_fundacion,id])
         return response.json({mensaje: 'Se actualizo el equipo'})   
+    }
+
+    async deleteEquipo({params,response}:HttpContextContract){
+        const id = params.id
+        const result = await client.query('delete from equipo where id=$1',[id])
+        return response.json({mensaje: 'El equipo se ha eliminado exitosamente'})
     }
 }
