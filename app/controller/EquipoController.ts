5ac2c0e41db7ae3d4c6a1fff8c9a7c28d125e098
Jesus <jesuscastrojaramillo346@gmail.com>
2025-05-22 15:38:23 -0500

    hola

---
+++ app/controller/EquipoController.ts
@@ -0,0 +1,24 @@
+import client from "../database/pgDatabase";
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class equiposcontroller{
+    async obtenerEquipos({request,response}){
+        const result = await client.query('select * from equipo')
+        console.log(result.rows)
+        return response.json({ mensaje: 'Estos son los equipos', datos: result.rows })
+    }
+
+    async crearEquipo({request,response}){
+        const {nombre,anio_fundacion}=request.body()
+        await client.query(`insert into equipo(nombre,anio_fundacion)values($1,$2)`,[nombre,anio_fundacion])
+        return response.json({mensaje:'Se creo el equipo', datos:{nombre,anio_fundacion},
+        })
+    }
+
+    async actualizarEquipo({params, request, response}){
+        const id = params.id
+        const {nombre,anio_fundacion} = request.body()
+        const result = await client.query('update equipo set nombre=$,anio_fundacion=$2 where id=$3',[nombre,anio_fundacion,id])
+        return response.json({mensaje: 'Se actualizo el equipo'})   
+    }
+}
