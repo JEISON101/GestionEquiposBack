e32f2c17190561e876b6ae8af04761318c6fcc71
JEISON101 <reyesjeison2006@gmail.com>
2025-06-05 16:17:32 -0500

    finalizada

--- app/controller/EquipoController.ts
+++ app/controller/EquipoController.ts
@@ -1,11 +1,10 @@
-import client from "../database/pgDatabase.js" 
+import client from "../database/PgDatabase.js"; 
 import { HttpContext } from "@adonisjs/core/http";
 
 export default class EquiposController{
 
-    async getEquipo({request,response}:HttpContext){
+    async getEquipo({response}:HttpContext){
         const result = await client.query('select * from equipo')
-        console.log(result.rows)
         return response.json({ mensaje: 'Estos son los equipos', datos: result.rows })
     }
 
@@ -19,7 +18,7 @@ export default class EquiposController{
     async putEquipo({params, request, response}:HttpContext){
         const id = params.id
         const {nombre,anio_fundacion} = request.body()
-        const result = await client.query('update equipo set nombre=$1,anio_fundacion=$2 where id_equipo=$3',[nombre,anio_fundacion,id])
+        await client.query('update equipo set nombre=$1,anio_fundacion=$2 where id_equipo=$3',[nombre,anio_fundacion,id])
         return response.json({mensaje: 'Se actualizo el equipo'})   
     }
 
